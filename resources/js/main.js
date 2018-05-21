88120f4d3b34aff334a0b59024b628ad0f382cea
vi <37302590+vi109@users.noreply.github.com>
2018-05-21 22:09:42 +0200

    Added background video and images

--- resources/js/main.js
+++ resources/js/main.js
@@ -9,3 +9,18 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
+
+// video
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "&#9724;";
+  } else {
+    video.pause();
+    btn.innerHTML = "&#9654;";
+  }
+}
